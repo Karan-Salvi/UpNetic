0853e9bb348111b781613de633f68025696912d9
Karan <karansalviwork@gmail.com>
2025-08-03 14:00:18 +0530

    fix:Setup enviroment variables

--- Backend/server.js
+++ Backend/server.js
@@ -60,7 +60,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? [FRONTEND_URI]
+        ? [process.env.FRONTEND_URI]
         : ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   })
